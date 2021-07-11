ffd0668b9074d052ad34664ab3e096c2580ef19e
Kohei KONISHI <konishi2911@icloud.com>
2021-07-11 12:45:26 +0900

    Initial Commit

---
+++ AccountBook/AccountBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountBookApp.swift
+//  AccountBook
+//
+//  Created by Kohei KONISHI on 2021/07/11.
+//
+
+import SwiftUI
+
+@main
+struct AccountBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
