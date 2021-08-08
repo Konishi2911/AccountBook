3d53c59013ac3ced5562f89f3c1f6bdde38336e2
Kohei KONISHI <konishi2911@icloud.com>
2021-08-08 10:10:58 +0900

    [ADD] Created a form model of Detail View

--- AccountBook/DetailView/DetailView.swift
+++ AccountBook/DetailView/DetailView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct DetailView: View {
+    private let cal_: Calendar = .autoupdatingCurrent
+    private let ref_: AccountDatabase
+    @State private var duration: DateInterval = .init()
+    
+    init (ref: AccountDatabase, duration: DateInterval) {
+        self.ref_ = ref
+        self.duration = duration
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Title").font(.title).fontWeight(.bold)
             HStack {
-                self.totalAmounts(label: "Incomes", value: 100000)
-                self.totalAmounts(label: "Outlay", value: 20000)
+                Text(self.title).font(.title).fontWeight(.bold)
+                    .padding()
+                Spacer()
+                Button(action: { self.shiftDuration(value: -1) }) {
+                    Image(systemName: "chevron.left")
+                }
+                .buttonStyle(PlainButtonStyle())
+                Button(action: { self.shiftDuration(value: 1) }) {
+                    Image(systemName: "chevron.right")
+                }
+                .buttonStyle(PlainButtonStyle())
+                .padding([.trailing])
+            }
+            HStack {
+                self.totalAmounts(label: "Incomes", value: self.incomeAmounts)
+                self.totalAmounts(label: "Outlay", value: self.outlayAmounts)
             }
         }
     }
@@ -32,10 +54,49 @@ struct DetailView: View {
                 .padding([.horizontal, .bottom])
         }
     }
+    func shiftDuration(value: Int) {
+        let newStartDate = self.cal_.date(
+            byAdding: .month, value: value, to: self.duration.start
+        )!
+        self.duration = DateInterval(
+            start: newStartDate,
+            end: self.cal_.date(byAdding: .month, value: 1, to: newStartDate)!
+        )
+    }
+    
+    var title: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = DateFormatter.dateFormat(
+            fromTemplate: "yyyMMMM", options: 0, locale: nil
+        )
+        
+        return formatter.string(from: self.duration.start)
+    }
+    var incomeAmounts: Int {
+        let recs = self.ref_.getRecords()
+            .filtered(by: DateFilter(start: self.duration.start, end: self.duration.end))
+            .filtered(by: CategoryFilter(category: AccountCategory(type: .income)))
+            .getArray()
+        return Int(recs.reduce(0) {$0 + $1.amounts})
+    }
+    var outlayAmounts: Int {
+        let recs = self.ref_.getRecords()
+            .filtered(by: DateFilter(start: self.duration.start, end: self.duration.end))
+            .filtered(by: CategoryFilter(category: AccountCategory(type: .outlay)))
+            .getArray()
+        return Int(recs.reduce(0) {$0 + $1.amounts})
+    }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        let cal = Calendar.current
+        DetailView(
+            ref: AccountDatabase.mock(),
+            duration: DateInterval(
+                start: cal.date(from: DateComponents(year: 2021, month: 5))!,
+                end: cal.date(from: DateComponents(year: 2021, month: 6))!
+            )
+        )
     }
 }
