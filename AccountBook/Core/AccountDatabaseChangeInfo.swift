a98d1f4985d9384600ff6e5cba99592502f73c35
Kohei KONISHI <konishi2911@icloud.com>
2021-09-22 13:17:53 +0900

    [ADD] Add AccountDatabaseChangeInfo to notify informations of changing the database.

---
+++ AccountBook/Core/AccountDatabaseChangeInfo.swift
@@ -0,0 +1,13 @@
+//
+//  AccountDatabaseChangeInfo.swift
+//  AccountBook
+//
+//  Created by Kohei KONISHI on 2021/09/22.
+//
+
+import Foundation
+
+struct AccountDatabaseChangeInfo {
+    let prevRecord: AccountRecord?
+    let newRecord: AccountRecord?
+}
